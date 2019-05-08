7e56b9e0554a4fc677fe8f23e373ddc386173c41
elisim <elisimhayev@gmail.com>
2019-05-08 13:17:24 +0300

    added all non-terminals functions

--- Parser/parser.h
+++ Parser/parser.h
@@ -1,16 +1,31 @@
 #ifndef PARSER_H
 #define PARSER_H
-
-#include "tokens_names.h"
 #define SUCCESS 0
 #define FAIL 1
+
+#include "tokens_names.h"
+
+int error();
+
+int parser();
 
 /* === Non-Terminals Functions === */
 int PROGRAM();
 int VAR_DEFINITIONS();
-int STATEMENTS();
+int VAR_DEFINITION();
+int TYPE();
+int VARIABLES_LIST();
+int VARIABLES_LIST_TEMP();
+int VARIABLE();
 int FUNC_DEFINITIONS();
-int error();
-
-int parser();
+int FUNC_DEFINITIONS_TEMP();
+int FUNC_DEFINITION();
+int RETURNED_TYPE();
+int PARAM_DEFINITIONS();
+int STATEMENTS();
+int STATEMENT();
+int BLOCK();
+int FUNCTION_CALL();
+int PARAMETERS_LIST();
+int EXPRESSION();
 #endif
